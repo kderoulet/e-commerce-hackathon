f1e8a2ef0f7d2a0f35a6b04978f4db3bb3d8f33c
Kevin de Roulet <kderoulet@yahoo.com>
2017-12-08 10:27:57 -0800

    hashes out server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+});
+
+var port = 3001;
+app.listen(port, function() {
+    console.log(`Express app running on port ${port}`)
+})
